fdc386b2ea89ccbdc062fd31d33277ae58f631eb
CHENGUANGTAO <792607724@qq.com>
2022-01-21 15:17:46 +0800

    Commit a new java project

---
+++ src/android/java/Cat.java
@@ -0,0 +1,37 @@
+package android.java;
+
+public class Cat {
+
+    public String name;
+    public String color;
+    public int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public void heihei() {
+        System.out.println("hei hei!!!");
+        System.out.printf("My name is %s, and i like %s color, now i'm %s years old\n", this.name, this.color, this.age);
+    }
+
+    public static void main(String[] args) {
+        haha(123);
+        Cat c = new Cat("Bruce", "Yellow with Gray", 2);
+        c.heihei();
+        haha(12, 13);
+    }
+
+    public static void haha(int a) {
+        System.out.println("ha ha!!!" + a);
+
+    }
+
+    public static void haha(int a, int b) {
+        System.out.println("haha!!!" + a + b);
+    }
+
+
+}
--- src/android/java/HelloWorld.java
+++ src/android/java/HelloWorld.java
@@ -3,8 +3,48 @@ package android.java;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("KO");
+//        System.out.println("Hello World!");
+//        System.out.println("KO");
+//        int a = 12;
+//        System.out.println(a);
+//        double b = 12.234234;
+//        System.out.println(b);
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(i);
+//            System.out.println("Hello World!!!");
+//        }
+//        int j = 0;
+//        for (; j < 10; j++) {
+//            System.out.println(j);
+//        }
+        int a = 2;
+        int b = 2;
+        if (a < b) {
+            System.out.printf("a<b,a=%s,b=%s", a, b);
+        } else if (a == b) {
+            System.out.println("A == B");
+        } else {
+            System.out.println("OK BOOM!!!");
+        }
+        int i = 1;
+        switch (i) {
+            case 0:
+                System.out.printf("i is %s", i);
+                break;
+            case 1:
+                System.out.printf("i is %s", i);
+                break;
+            default:
+                System.out.printf("i is not ok : %s", i);
+                break;
+        }
+        int[] array;
+        array = new int[]{1, 2, 3, 4, 5};
+        for (int j = 0; j < array.length + 1; j++) {
+            System.out.println(j);
+        }
+        System.out.println(array[0]);
     }
 
 }
